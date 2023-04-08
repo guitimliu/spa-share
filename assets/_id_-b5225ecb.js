d29d76847776d7ce77ce7ec628c0f4a8fde3ff93
Gui <guitimliu@gmail.com>
2023-04-08 23:03:31 +0800

    deploy

---
+++ assets/_id_-b5225ecb.js
@@ -0,0 +1,1 @@
+import{_ as a,o as r,a as t,b as o,t as s}from"./index-0dc38f4c.js";const n={};function c(e,_){return r(),t("div",null,[o("h1",null,"路由 id "+s(e.$route.params.id),1)])}const l=a(n,[["render",c]]);export{l as default};
